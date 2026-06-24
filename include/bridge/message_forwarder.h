35768df97f61ed24a2232d3b35af33b176b997a6
MatteV02 <veroni.matteo@icloud.com>
2026-06-24 15:59:31 +0200

    fixed header order in message_forwarder.h

--- include/bridge/message_forwarder.h
+++ include/bridge/message_forwarder.h
@@ -1,8 +1,9 @@
 #ifndef MESSAGE_FORWARDER_H_
 #define MESSAGE_FORWARDER_H_
 
-#include <mosquitto.h>
+#include <mosquitto_broker.h>
 #include <mosquitto_plugin.h>
+#include <mosquitto.h>
 
 struct mosquitto * start_forwarder(char* client_id, char* host, int port);
 int forward_message(struct mosquitto *ext_client, struct mosquitto_evt_acl_check* msg);
